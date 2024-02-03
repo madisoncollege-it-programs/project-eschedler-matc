b0fa01c022376d76308b3b8104138378bca154a6
Eric <eschedler@madisoncollege.edu>
2024-02-03 13:04:39 -0600

    Submitting Milestone 1 for Grading.

---
+++ apache_log_analyzer.py
@@ -0,0 +1,7 @@
+#Author: Eric Schedler
+#Email:eschedler@madisoncollege.edu
+#Description:
+program_description = "Welcome to my simple program!  This program will ask for user input and then notify the user if the input has been recieved."
+print(program_description)
+user_input = input("would you like to continue?  ('y' for yes and 'n' for no):")
+print(f"You have entered: {user_input}")
